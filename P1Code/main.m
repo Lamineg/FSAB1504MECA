846ce19382d00ea75e966be57a2bdc50c3008082
Baawaah <son.tran.be@gmail.com>
2016-02-09 15:11:20 +0100

    init of the structure data; il manque encore q, qd, et qdd

--- P1Code/main.m
+++ P1Code/main.m
@@ -1,1 +1,10 @@
-% For now it's only a README
+% For now it's only nothing
+
+% Initialisation de la structure data
+data.m   = [ 16.0, 4.5, 1.5, 1.0, 2.0 ]; % [Kg]
+data.dOA = [ 100, 250, ]               ; % [mm]
+data.q   = [   0,   0,    0,   0,   0 ]; % q=(x1,theta1,x2,theta2,theta3)
+data.qd  = [   0,   0,    0,   0,   0 ];
+data.dqq = [   0,   0,    0,   0,   0 ];
+data.qu  = [4]                         ;
+data.qv  = [1 2 3 5]                   ;
