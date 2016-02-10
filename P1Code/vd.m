1e7e6a9a0cd167691c9a6c438865c7514b6bd835
Demeyj <julien.demey@student.uclouvain.be>
2016-02-10 20:59:07 +0100

    little tip for the next update

--- P1Code/vd.m
+++ P1Code/vd.m
@@ -5,7 +5,7 @@ function [vd] = vd(ud,jac)
 Jv = jac(:,qv);
 Ju = jac(:,qu);
 b = -Ju*ud; %qd(qu)
-vd = Jv\b;
+vd = Jv\b; %peut etre faire comme dans NR au lieu de vd, mettre data.qd(qv) avec data comme sortie
 
 end
 
