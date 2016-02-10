0d7fe8d356579b0ff8934c25e0e0e36b60b42a25
Demeyj <julien.demey@student.uclouvain.be>
2016-02-10 21:06:06 +0100

    need to choose

--- P1Code/vd.m
+++ P1Code/vd.m
@@ -9,3 +9,19 @@ vd = Jv\b; %peut etre faire comme dans NR au lieu de vd, mettre data.qd(qv) avec
 
 end
 
+
+%OU ALORS...? %Need to check
+
+
+function [vd] = vd(data)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+[~, jac] = QuickRManuel_cons_hJ(data);
+Jv = jac(:,qv);
+Ju = jac(:,qu);
+ud=data.qd(qu);
+b = -Ju*ud; 
+vd = Jv\b; 
+
+end
