d6b270afb3b77d6c91277cd8df7f583386624f09
Lamineg <guillaume.lamine@student.uclouvain.be>
2016-02-17 22:59:10 +0100

    Update QuickRManuel_cons_jdqd.m

--- P1Code/QuickRManuel_cons_jdqd.m
+++ P1Code/QuickRManuel_cons_jdqd.m
@@ -14,19 +14,18 @@ x2_point     = data.qd(3);
 theta2_point = data.qd(4);
 theta3_point = data.qd(5);
 
-L2     = 530;
-L4     = 100;
-L5     = 100;
+L2     = 0.530;
+L4     = 0.100;
+L5     = 0.100;
 
-%dérivée de la Jacobienne
-Jpoint = [0 -x2_point*sin(theta1)-x2*theta1_point*cos(theta1)...
-                   -theta1_point*sin(theta1) L4*theta2_point*cos(theta2) 0;
-          0 -x2_point*cos(theta1)-x2*theta1_point*sin(theta1)...
-                    theta1_point*cos(theta1) L4*theta2_point*sin(theta2) 0;
-          0 -L2*theta1_point^2*cos(theta1) 0 0 L5*theta3_point_cos(theta3);
-          0 -L2*theta1_point^2*sin(theta1) 0 0 L5*theta3_point_cos(theta3)];
+%derivee de la Jacobienne
+Jpoint = [0, -x2_point*sin(theta1)-x2*theta1_point*cos(theta1),...
+                   -theta1_point*sin(theta1), L4*theta2_point*cos(theta2), 0;
+          0, x2_point*cos(theta1) - x2*theta1_point*sin(theta1),...
+                    theta1_point*cos(theta1), L4*theta2_point*sin(theta2), 0;
+          0, -L2*theta1_point*cos(theta1), 0, 0, L5*theta3_point*cos(theta3);
+          0, -L2*theta1_point*sin(theta1), 0, 0, L5*theta3_point*sin(theta3)];
       
-Jdqd=Jpoint*data.qd;
+Jdqd = Jpoint * data.qd;
 
 end
-
